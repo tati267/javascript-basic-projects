2d3beed0c8d69b9e056af935a40e6f9e14641228
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-07-09 22:15:12 +1000

    finished project review №3

--- 3-reviews/setup/app.js
+++ 3-reviews/setup/app.js
@@ -37,3 +37,61 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+//select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+//set starting item
+let currentItem = 0;
+
+//load initial item 
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  //load initial item--  img.src = item.img;
+  //load initial item--- author.textContent = item.name;
+  //load initial item--- job.textContent = item.job;
+  //load initial item--- info.textContent = item.text;
+  showPerson(currentItem);
+});
+
+//show person based on item+add function
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next person
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  };
+  showPerson();
+})
+
+
+//show prev person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  };
+  showPerson();
+})
+
+//show random person
+randomBtn.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length)
+  console.log(currentItem);
+  showPerson();
+})
