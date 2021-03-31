56b3b7e97bd5c4628b290f77a3cef34feea88616
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-03-31 20:53:53 +1100

    scroll: smooth scroll from top links to sections

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -37,7 +37,37 @@ window.addEventListener('scroll', function () {
   const navHeight = navbar.getBoundingClientRect().height;
 
   scrollHeight > navHeight ? navbar.classList.add('fixed-nav') : navbar.classList.remove('fixed-nav');
-})
-// ********** smooth scroll ************
 
+  // ********** up button  ************
+  scrollHeight > 500 ? topLink.classList.add('show-link') : topLink.classList.remove('show-link');
+})
+
+// ********** smooth scroll ************
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    e.preventDefault();
+    //navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //calculate heigth
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+    linksContainer.style.height = 0;
+  })
+})
 // calculate heights
